143559fbf6b800c15e3cec330b52c9bfceeb4d53
bendunhampdx <bendunhampdx@gmail.com>
2021-11-30 15:59:13 -0800

    add icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a Discord clone!!</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat */}
     </div>
   );
 }
