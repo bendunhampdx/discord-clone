8a33ca3c0b27426a24dba7a0edca88575841c2a3
bendunhampdx <bendunhampdx@gmail.com>
2021-12-01 10:52:54 -0800

    add chat componet

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import './Sidebar.css'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChannel from './SidebarChannel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+import { Avatar } from '@material-ui/core';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
   return (
@@ -29,7 +36,34 @@ function Sidebar() {
         </div>
       </div>
 
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
 
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div> 
+      </div>
+
+      <div className="sidebar__profile">
+        <Avatar src="https://lh3.googleusercontent.com/ogw/ADea4I4KLL7jUpmS5x1BrVnv1dnOAQhNCqNXtyC9io0now=s64-c-mo"/>
+        <div className="sidebar__profileInfo">
+          <h3>@ben_riffs</h3>
+          <p>#thisIsMyId</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
     </div>
   );
 }
