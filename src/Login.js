acda20bf7417230c3bfdcab0a09c6ac2650c3757
bendunhampdx <bendunhampdx@gmail.com>
2021-12-01 16:22:57 -0800

    add login styles

--- src/Login.js
+++ src/Login.js
@@ -10,10 +10,9 @@ function Login() {
 
   return (
     <div className="login">
-      <h2>I am the login page</h2>
 
       <div className="login__logo">
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Faminoapps.com%2Fc%2Fmtg%2Fpage%2Fblog%2Fthe-official-mtg-amino-discord-server%2FZ6DF_BuYkZDg7E8Kr4PLM0l0222jRG&psig=AOvVaw2Sv3Sr2FnPcDLkgDMmFKbW&ust=1638489422097000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJD_7vTmw_QCFQAAAAAdAAAAABAJ" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPy9nuR2_3f1QRTeU5t0IJvpI0X8rFma4WOQ&usqp=CAU" alt="" />
       </div>
 
       <Button onClick={signIn}>Sign In</Button>
