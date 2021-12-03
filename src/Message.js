5dd8dc890c3e9eee3985671d56e71981b20138c2
bendunhampdx <bendunhampdx@gmail.com>
2021-12-03 10:27:13 -0800

    firebase deploy

--- src/Message.js
+++ src/Message.js
@@ -2,16 +2,19 @@ import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Message.css'
 
-function Message() {
+function Message({timestamp, user, message}) {
   return (
     <div className="message">
-      <Avatar />
+      <Avatar src={user.photo}/>
       <div className="message__info">
-        <h4>ben_riffs
-          <span className="message__timestamp">this is a timestamp</span>
+        <h4>
+          {user.displayName}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
 
-        <p>This is a message</p>
+        <p>{message}</p>
       </div>
     </div>
   )
